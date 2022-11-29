2c4350c2adfae24ad607716cb808bdd5cce03d03
Sudhanshu <perfectsudh@gmail.com>
2022-11-29 14:38:43 +0530

    Add msw for api mocking

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,12 @@
+// https://nextjs.org/docs/basic-features/eslint#lint-staged
+
+const path = require("path")
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(" --file ")}`
+
 module.exports = {
-  // Type check TypeScript files
-  "**/*.(ts|tsx)": () => "yarn tsc --noEmit",
-
-  // Lint & Prettify TS and JS files
-  "**/*.(ts|tsx|js)": (filenames) => [
-    `yarn eslint ${filenames.join(" ")}`,
-    `yarn prettier --write ${filenames.join(" ")}`,
-  ],
-
-  // Prettify only Markdown and JSON files
-  "**/*.(md|json)": (filenames) =>
-    `yarn prettier --write ${filenames.join(" ")}`,
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
 }
