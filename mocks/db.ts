fc907db5e1f19d86ff382e239b372c993a88220a
Sudhanshu <perfectsudh@gmail.com>
2022-11-30 17:43:43 +0530

    Remove DB seed

--- mocks/db.ts
+++ mocks/db.ts
@@ -1,37 +1,3 @@
 import { Resource } from "../models";
 
-function seedResources(): Record<string, Resource> {
-  return {
-    "Folder 1": {
-      name: "Folder 1",
-      type: "folder",
-      children: {
-        "Something": {
-          name: "Something",
-          type: "folder",
-          children: {
-            "Something Else": {
-              name: "Something Else",
-              type: "file"
-            }
-          }
-        }
-      }
-    },
-    "Folder 2": {
-      name: "Folder 2",
-      type: "folder",
-      children: {}
-    },
-    "File1.txt": {
-      name: "File1.txt",
-      type: "file"
-    },
-    "File2.txt": {
-      name: "File2.txt",
-      type: "file"
-    }
-  }
-}
-
-export const db: Record<string, Resource> = { ...seedResources() }
+export const db: Record<string, Resource> = {}
