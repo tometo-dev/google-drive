616d0438fc1ee844292cedae97e366cfd9485d00
Sudhanshu <perfectsudh@gmail.com>
2022-12-01 16:37:13 +0530

    Use BASE_URL for base path instead of VERCEL_URL

--- utils/getApiUrl.ts
+++ utils/getApiUrl.ts
@@ -1,5 +1,5 @@
 function getBaseUrl() {
-  return process.env.VERCEL_URL ?? `http://localhost:3000`
+  return process.env.NEXT_PUBLIC_BASE_URL ?? `http://localhost:3000`
 }
 
 export function getApiUrl() {
